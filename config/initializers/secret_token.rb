c71f5b517228707ebd930b5f10675c555913e702
Jeff Wigal <jeff@assignr.com>
2012-01-20 13:42:49 -0500

    default spree store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmcommeastStore::Application.config.secret_token = '2543077f1421796c304e4c73d8b1ec2d1958c8b477048d581fc589ee49bbc98f105d417f2aa949dfef77b6a73c1b35ef5857359a75f0c7878098e351fec8782a'
